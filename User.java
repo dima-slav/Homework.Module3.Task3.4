2847117b9e9f0ad4529fb235fff3a8ca013bcc1f
dima-slav <dzirtdourden4@gmail.com>
2016-11-25 00:15:20 +0200

    Update User.java
    User commited

--- User.java
+++ User.java
@@ -4,50 +4,46 @@ package Task3_4;
  * Created by Дима on 24.11.2016.
  */
 public class User {
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
 
-        private String name;
-        private int balance;
-        private int monthsOfEmployment;
-        private String companyName;
-        private int salary;
-        private String currency;
-
-        public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
-            this.name = name;
-            this.balance = balance;
-            this.monthsOfEmployment = monthsOfEmployment;
-            this.companyName = companyName;
-            this.salary = salary;
-            this.currency = currency;
-
-        }
-        void paySalary(){
-            balance+=salary;
-        }
-
-        double withdraw(int summ){
-            double withdraw;
-            double comission;
-            if (summ < 1000) {
-                comission = summ * 0.05;
-
-            } else {
-                comission = summ * 0.1;
-
-
-            }
-            withdraw = summ + comission;
-            double bal = (double) balance;
-            bal = bal - withdraw;
-            return bal;
-        }
-
-    void companyNameLengt() {
-        System.out.println(companyName.length());
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
     }
 
-    void monthIncreaser(int addMonth) {
-        monthsOfEmployment += addMonth;
+    public void paySalary(int salary) {
+        this.balance += salary;
+        System.out.println(this.balance);
+    }
+
+    public void withdraw(int summ) {
+        double comission = summ < 1000 ? summ * 0.1 : summ * 0.05;
+
+        if (this.balance < summ + comission) {
+            return;
+        } else {
+            this.balance -= summ + comission;
+        }
+
+        System.out.println(this.balance);
+    }
+
+    public int companyNameLenght() {
+        return this.companyName.length();
+    }
+
+    public void monthIncreaser(int addMonth) {
+        this.monthsOfEmployment += addMonth;
+        System.out.println(addMonth);
     }
 
     public String getName() {
